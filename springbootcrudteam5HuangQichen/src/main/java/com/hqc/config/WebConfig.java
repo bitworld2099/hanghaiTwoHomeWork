2686077d5475130f3e2fd7b02bceee224775dc7f
rari <852288794@qq.com>
2020-01-03 21:24:40 +0800

    测试

--- springbootcrudteam5HuangQichen/src/main/java/com/hqc/config/WebConfig.java
+++ springbootcrudteam5HuangQichen/src/main/java/com/hqc/config/WebConfig.java
@@ -7,10 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        //注册视图控制器
-        // url为："/crud"
-        // 视图名为："crud"
-        // 对应页面：templates/crud.html
         registry.addViewController("/crud").setViewName("crud");
     }
 }
